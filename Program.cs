b1ec85d777aa53a48a81f880e48f330fd0f0262f
[Samir Jabrayilzada] <[samirej@code.edu.az]>
2022-12-21 21:42:08 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NewProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
